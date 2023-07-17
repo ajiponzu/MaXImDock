b6379669617670831117efca4a2e2f17a18a648a
ajiponzu <cpjvmm@outlook.jp>
2023-07-18 03:42:50 +0900

    fix: package refactor

--- MaXImDock/MaXImDock/MainWindow.xaml.cpp
+++ MaXImDock/MaXImDock/MainWindow.xaml.cpp
@@ -9,9 +9,6 @@
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace winrt::MaXImDock::implementation
 {
 	MainWindow::MainWindow()
