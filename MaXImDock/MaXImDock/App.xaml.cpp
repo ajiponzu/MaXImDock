87321ffd7b15eda8c784cc5f85525d67ea3a9164
ajiponzu <cpjvmm@outlook.jp>
2025-09-30 23:47:36 +0900

    add: mutex for forbid multiple maximdock launch

--- MaXImDock/MaXImDock/App.xaml.cpp
+++ MaXImDock/MaXImDock/App.xaml.cpp
@@ -1,8 +1,10 @@
 ﻿#include "App.xaml.h"
-#include "MainWindow.xaml.h"
-#include "AppDataModel.h"
 
 #include <WinUser.h>
+#include <Windows.h>
+
+#include "AppDataModel.h"
+#include "MainWindow.xaml.h"
 
 using namespace winrt;
 using namespace Windows::Foundation;
@@ -21,165 +23,180 @@ static constexpr double g_INCH_PER_MILLIMETER = 25.4;
 static bool g_is_resized = false;
 static bool g_is_resizing = false;
 
-/// <summary>
-/// Initializes the singleton application object.  This is the first line of authored code
-/// executed, and as such is the logical equivalent of main() or WinMain().
-/// </summary>
-App::App()
-{
-	InitializeComponent();
+HANDLE g_hMutex = nullptr;
 
-#if defined _DEBUG && !defined DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION
-	UnhandledException([this](IInspectable const&, UnhandledExceptionEventArgs const& e)
-		{
-			if (IsDebuggerPresent())
-			{
-				auto errorMessage = e.Message();
-				__debugbreak();
-			}
-		});
+/// <summary>
+/// Initializes the singleton application object.  This is the first line of
+/// authored code executed, and as such is the logical equivalent of main() or
+/// WinMain().
+/// </summary>
+App::App() {
+  InitializeComponent();
+
+#if defined _DEBUG && \
+    !defined DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION
+  UnhandledException(
+      [this](IInspectable const&, UnhandledExceptionEventArgs const& e) {
+        if (IsDebuggerPresent()) {
+          auto errorMessage = e.Message();
+          __debugbreak();
+        }
+      });
 #endif
 }
 
+App::~App() {
+  if (g_hMutex) {
+    ::CloseHandle(g_hMutex);
+  }
+}
+
 /// <summary>
-/// Invoked when the application is launched normally by the end user.  Other entry points
-/// will be used such as when the application is launched to open a specific file.
+/// Invoked when the application is launched normally by the end user.  Other
+/// entry points will be used such as when the application is launched to open a
+/// specific file.
 /// </summary>
 /// <param name="e">Details about the launch request and process.</param>
-void App::OnLaunched(LaunchActivatedEventArgs const&)
-{
-	InitWindow();
+void App::OnLaunched(LaunchActivatedEventArgs const&) {
+  g_hMutex = ::CreateMutexW(nullptr, FALSE, L"MaXImDock_Mutex");
+  if (!g_hMutex) {
+    ::MessageBoxW(nullptr, L"Mutexの作成に失敗しました", L"エラー", MB_OK);
+    exit(1);
+  }
+  if (GetLastError() == ERROR_ALREADY_EXISTS) {
+    ::CloseHandle(g_hMutex);
+    exit(0);
+  }
+
+  InitWindow();
 }
 
-winrt::IAsyncAction winrt::MaXImDock::implementation::App::InitSystem()
-{
-	co_await MaXImDockModel::AppDataModel::ReadSettingJson();
+winrt::IAsyncAction winrt::MaXImDock::implementation::App::InitSystem() {
+  co_await MaXImDockModel::AppDataModel::ReadSettingJson();
 
-	InitWindow();
+  InitWindow();
 }
 
-void winrt::MaXImDock::implementation::App::InitWindow()
-{
-	if (g_is_resizing && !g_is_resized)
-		return;
+void winrt::MaXImDock::implementation::App::InitWindow() {
+  if (g_is_resizing && !g_is_resized) return;
 
-	m_window = make<MainWindow>();
+  m_window = make<MainWindow>();
 
-	SetWindowStyle();
-	SetWindowSizeAndPos();
-	m_window.AppWindow().MoveAndResize(m_windowRect);
+  SetWindowStyle();
+  SetWindowSizeAndPos();
+  m_window.AppWindow().MoveAndResize(m_windowRect);
 
-	const auto size_changed_handler = [&](winrt::IInspectable const& /*sender*/, winrt::Microsoft::UI::Xaml::WindowSizeChangedEventArgs const& /*args*/)
-	{
-		g_is_resizing = true;
+  const auto size_changed_handler =
+      [&](winrt::IInspectable const& /*sender*/,
+          winrt::Microsoft::UI::Xaml::
+              WindowSizeChangedEventArgs const& /*args*/) {
+        g_is_resizing = true;
 
-		if (!g_is_resized)
-			g_is_resized = true;
-		else
-			g_is_resized = false;
+        if (!g_is_resized)
+          g_is_resized = true;
+        else
+          g_is_resized = false;
 
-		m_windowActivateHandlerStatus.Close();
-		::Sleep(g_SLEEP_TIME);
-		InitWindow();
-		g_is_resizing = false;
-	};
-	m_window.SizeChanged(size_changed_handler);
+        m_windowActivateHandlerStatus.Close();
+        ::Sleep(g_SLEEP_TIME);
+        InitWindow();
+        g_is_resizing = false;
+      };
+  m_window.SizeChanged(size_changed_handler);
 
-	m_windowActivateHandlerStatus = IAsyncAction{ Async_WindowActivateHandler() };
+  m_windowActivateHandlerStatus = IAsyncAction{Async_WindowActivateHandler()};
 }
 
-void winrt::MaXImDock::implementation::App::SetWindowSizeAndPos()
-{
-	::HWND hwnd = nullptr;
-	winrt::com_ptr<IWindowNative> window_native = m_window.as<IWindowNative>(); // win32ネイティブウィンドウの取得
-	window_native->get_WindowHandle(&hwnd); // ウィンドウハンドル取得
+void winrt::MaXImDock::implementation::App::SetWindowSizeAndPos() {
+  ::HWND hwnd = nullptr;
+  winrt::com_ptr<IWindowNative> window_native =
+      m_window.as<IWindowNative>();        // win32ネイティブウィンドウの取得
+  window_native->get_WindowHandle(&hwnd);  // ウィンドウハンドル取得
 
-	// 取得するdpiはhwndのパラメータなので，同一hwndでのdpi変化は感知しない. だからwindowを作り直す
-	const auto dpi_rate = (double)::GetDpiForWindow(hwnd) / USER_DEFAULT_SCREEN_DPI;
+  // 取得するdpiはhwndのパラメータなので，同一hwndでのdpi変化は感知しない.
+  // だからwindowを作り直す
+  const auto dpi_rate =
+      (double)::GetDpiForWindow(hwnd) / USER_DEFAULT_SCREEN_DPI;
 
-	::SystemParametersInfo(SPI_GETWORKAREA, NULL, &m_rcDispRect, NULL);
-	const auto x_disp_rate = (double)m_rcDispRect.right / g_BASE_DISP_WID;
-	const auto y_disp_rate = (double)m_rcDispRect.bottom / g_BASE_DISP_HIGH;
+  ::SystemParametersInfo(SPI_GETWORKAREA, NULL, &m_rcDispRect, NULL);
+  const auto x_disp_rate = (double)m_rcDispRect.right / g_BASE_DISP_WID;
+  const auto y_disp_rate = (double)m_rcDispRect.bottom / g_BASE_DISP_HIGH;
 
-	m_activateBorderX = m_rcDispRect.right - 1;
-	m_windowRect.Width = (int32_t)((double)m_rcDispRect.right * 0.075 * dpi_rate / x_disp_rate); // display比率はなぜか割る
-	m_windowRect.Height = (int32_t)((double)m_rcDispRect.bottom * 0.45 * dpi_rate / y_disp_rate);
-	m_windowRect.X = (int32_t)(m_activateBorderX - m_windowRect.Width);
-	m_windowRect.Y = (int32_t)(m_rcDispRect.bottom - m_windowRect.Height);
+  m_activateBorderX = m_rcDispRect.right - 1;
+  m_windowRect.Width = (int32_t)((double)m_rcDispRect.right * 0.075 * dpi_rate /
+                                 x_disp_rate);  // display比率はなぜか割る
+  m_windowRect.Height =
+      (int32_t)((double)m_rcDispRect.bottom * 0.45 * dpi_rate / y_disp_rate);
+  m_windowRect.X = (int32_t)(m_activateBorderX - m_windowRect.Width);
+  m_windowRect.Y = (int32_t)(m_rcDispRect.bottom - m_windowRect.Height);
 }
 
-void winrt::MaXImDock::implementation::App::SetWindowStyle()
-{
-	OverlappedPresenter overlapped_presenter(0);
-	overlapped_presenter = OverlappedPresenter::CreateForContextMenu();
-	overlapped_presenter.IsAlwaysOnTop(true); // 常に最前面に表示
-	m_window.AppWindow().SetPresenter(overlapped_presenter); // ウィンドウスタイル適用
-	m_window.AppWindow().IsShownInSwitchers(false);
+void winrt::MaXImDock::implementation::App::SetWindowStyle() {
+  OverlappedPresenter overlapped_presenter(0);
+  overlapped_presenter = OverlappedPresenter::CreateForContextMenu();
+  overlapped_presenter.IsAlwaysOnTop(true);  // 常に最前面に表示
+  m_window.AppWindow().SetPresenter(
+      overlapped_presenter);  // ウィンドウスタイル適用
+  m_window.AppWindow().IsShownInSwitchers(false);
 }
 
-bool winrt::MaXImDock::implementation::App::CheckCursorEntered(const ::POINT& cursor_pos)
-{
-	const bool was_entered =
-		cursor_pos.x >= m_activateBorderX
-		&& cursor_pos.x <= (m_windowRect.X + m_windowRect.Width * 3)
-		&& cursor_pos.y >= m_windowRect.Y;
+bool winrt::MaXImDock::implementation::App::CheckCursorEntered(
+    const ::POINT& cursor_pos) {
+  const bool was_entered =
+      cursor_pos.x >= m_activateBorderX &&
+      cursor_pos.x <= (m_windowRect.X + m_windowRect.Width * 3) &&
+      cursor_pos.y >= m_windowRect.Y;
 
-	return was_entered;
+  return was_entered;
 }
 
-bool winrt::MaXImDock::implementation::App::CheckCursorOnWindow(const::POINT& cursor_pos)
-{
-	const bool is_on_window =
-		cursor_pos.x >= m_windowRect.X
-		&& cursor_pos.x <= (m_windowRect.X + m_windowRect.Width * 3)
-		&& cursor_pos.y >= m_windowRect.Y;
+bool winrt::MaXImDock::implementation::App::CheckCursorOnWindow(
+    const ::POINT& cursor_pos) {
+  const bool is_on_window =
+      cursor_pos.x >= m_windowRect.X &&
+      cursor_pos.x <= (m_windowRect.X + m_windowRect.Width * 3) &&
+      cursor_pos.y >= m_windowRect.Y;
 
-	return is_on_window;
+  return is_on_window;
 }
 
-bool winrt::MaXImDock::implementation::App::CheckWindowPosIncorrective()
-{
-	const auto app_window_pos = m_window.AppWindow().Position();
-	const auto is_incorrect =
-		app_window_pos.X < m_rcDispRect.left
-		|| app_window_pos.X > m_rcDispRect.right
-		|| app_window_pos.Y < m_rcDispRect.top
-		|| app_window_pos.Y > m_rcDispRect.bottom;
+bool winrt::MaXImDock::implementation::App::CheckWindowPosIncorrective() {
+  const auto app_window_pos = m_window.AppWindow().Position();
+  const auto is_incorrect = app_window_pos.X < m_rcDispRect.left ||
+                            app_window_pos.X > m_rcDispRect.right ||
+                            app_window_pos.Y < m_rcDispRect.top ||
+                            app_window_pos.Y > m_rcDispRect.bottom;
 
-	return is_incorrect;
+  return is_incorrect;
 }
 
-winrt::IAsyncAction winrt::MaXImDock::implementation::App::Async_WindowActivateHandler()
-{
-	co_await winrt::resume_background();
+winrt::IAsyncAction
+winrt::MaXImDock::implementation::App::Async_WindowActivateHandler() {
+  co_await winrt::resume_background();
 
-	bool is_waited_activate = false;
+  bool is_waited_activate = false;
 
-	::POINT cursor_pos;
-	::GetCursorPos(&cursor_pos);
+  ::POINT cursor_pos;
+  ::GetCursorPos(&cursor_pos);
 
-	while (true)
-	{
-		::Sleep(g_SLEEP_TIME);
-		::GetCursorPos(&cursor_pos);
-		SetWindowSizeAndPos();
+  while (true) {
+    ::Sleep(g_SLEEP_TIME);
+    ::GetCursorPos(&cursor_pos);
+    SetWindowSizeAndPos();
 
-		if (is_waited_activate && CheckCursorEntered(cursor_pos))
-		{
-			co_await wil::resume_foreground(m_window.DispatcherQueue());
-			if (CheckWindowPosIncorrective())
-				m_window.AppWindow().MoveAndResize(winrt::RectInt32{1, 1, 1, 1});
-			else
-				m_window.Activate();
-			co_await winrt::resume_background();
-			is_waited_activate = false;
-		}
-		else if (!CheckCursorOnWindow(cursor_pos))
-		{
-			co_await wil::resume_foreground(m_window.DispatcherQueue());
-			m_window.AppWindow().Hide();
-			co_await winrt::resume_background();
-			is_waited_activate = true;
-		}
-	}
+    if (is_waited_activate && CheckCursorEntered(cursor_pos)) {
+      co_await wil::resume_foreground(m_window.DispatcherQueue());
+      if (CheckWindowPosIncorrective())
+        m_window.AppWindow().MoveAndResize(winrt::RectInt32{1, 1, 1, 1});
+      else
+        m_window.Activate();
+      co_await winrt::resume_background();
+      is_waited_activate = false;
+    } else if (!CheckCursorOnWindow(cursor_pos)) {
+      co_await wil::resume_foreground(m_window.DispatcherQueue());
+      m_window.AppWindow().Hide();
+      co_await winrt::resume_background();
+      is_waited_activate = true;
+    }
+  }
 }
--- MaXImDock/MaXImDock/App.xaml.h
+++ MaXImDock/MaXImDock/App.xaml.h
@@ -2,60 +2,60 @@
 
 #include "App.xaml.g.h"
 
-namespace winrt::MaXImDock::implementation
-{
-	struct App : AppT<App>
-	{
-	private:
-		winrt::RectInt32 m_windowRect{}; // ウィンドウの座標・サイズ
-		::RECT m_rcDispRect{}; // ディスプレイのDPI情報
-		int m_activateBorderX = 0; // ウィンドウ表示の際の座標境界
-		winrt::Microsoft::UI::Xaml::Window m_window{ nullptr }; // ウィンドウ
-		winrt::IAsyncAction m_windowActivateHandlerStatus{ winrt::IAsyncAction() };
-	public:
-		App();
+namespace winrt::MaXImDock::implementation {
+struct App : AppT<App> {
+ private:
+  winrt::RectInt32 m_windowRect{};  // ウィンドウの座標・サイズ
+  ::RECT m_rcDispRect{};            // ディスプレイのDPI情報
+  int m_activateBorderX = 0;        // ウィンドウ表示の際の座標境界
+  winrt::Microsoft::UI::Xaml::Window m_window{nullptr};  // ウィンドウ
+  winrt::IAsyncAction m_windowActivateHandlerStatus{winrt::IAsyncAction()};
 
-		void OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&);
+ public:
+  App();
+  ~App();
 
-	private:
-		/// <summary>
-		/// 非同期的にシステム起動処理. co_awaitにより, 実行順を制御
-		/// </summary>
-		winrt::IAsyncAction InitSystem();
+  void OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&);
 
-		/// <summary>
-		/// ウィンドウ初期化部
-		/// </summary>
-		void InitWindow();
+ private:
+  /// <summary>
+  /// 非同期的にシステム起動処理. co_awaitにより, 実行順を制御
+  /// </summary>
+  winrt::IAsyncAction InitSystem();
 
-		/// <summary>
-		/// ウィンドウの表示位置・サイズを調整
-		/// </summary>
-		void SetWindowSizeAndPos();
+  /// <summary>
+  /// ウィンドウ初期化部
+  /// </summary>
+  void InitWindow();
 
-		/// <summary>
-		/// ウィンドウのスタイルを設定
-		/// </summary>
-		void SetWindowStyle();
+  /// <summary>
+  /// ウィンドウの表示位置・サイズを調整
+  /// </summary>
+  void SetWindowSizeAndPos();
 
-		/// <summary>
-		/// カーソル座標の侵入判定を行う
-		/// </summary>
-		bool CheckCursorEntered(const ::POINT& cursor_pos);
+  /// <summary>
+  /// ウィンドウのスタイルを設定
+  /// </summary>
+  void SetWindowStyle();
 
-		/// <summary>
-		/// カーソル座標とウィンドウとの当たり判定を行う
-		/// </summary>
-		bool CheckCursorOnWindow(const ::POINT& cursor_pos);
+  /// <summary>
+  /// カーソル座標の侵入判定を行う
+  /// </summary>
+  bool CheckCursorEntered(const ::POINT& cursor_pos);
 
-		/// <summary>
-		/// ウィンドウ表示位置が正しいか検証する
-		/// </summary>
-		bool CheckWindowPosIncorrective();
+  /// <summary>
+  /// カーソル座標とウィンドウとの当たり判定を行う
+  /// </summary>
+  bool CheckCursorOnWindow(const ::POINT& cursor_pos);
 
-		/// <summary>
-		/// ウィンドウの表示・非表示処理を待機する
-		/// </summary>
-		winrt::IAsyncAction Async_WindowActivateHandler();
-	};
-}
+  /// <summary>
+  /// ウィンドウ表示位置が正しいか検証する
+  /// </summary>
+  bool CheckWindowPosIncorrective();
+
+  /// <summary>
+  /// ウィンドウの表示・非表示処理を待機する
+  /// </summary>
+  winrt::IAsyncAction Async_WindowActivateHandler();
+};
+}  // namespace winrt::MaXImDock::implementation
